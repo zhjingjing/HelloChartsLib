f0805d750c88d3a6fe9f59f4a4505304da35cdbc
zhjingjing <18337104423@163.com>
2017-04-06 15:14:57 +0800

    hellocharts库

---
+++ HelloChartsLib/src/lecho/lib/hellocharts/MainActivity.java
@@ -0,0 +1,22 @@
+package lecho.lib.hellocharts;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+//    super.onCreate(savedInstanceState);
+//    setContentView(R.layout.activity_main);
+//    
+//    SharedPreferences sharedPreferences=getSharedPreferences("hellocharts", Context.MODE_WORLD_READABLE);
+//    Editor editor=  sharedPreferences.edit();
+//    editor.putString("key","这是存下来的数据，你可以获取到吗？");
+//    editor.commit();
+  }
+}
